aac0348b268892588f0ddd143c96061d381c86ed
Edvin Eriksson <edvin.eriksson@elev.ga.ntig.se>
2024-09-10 13:22:22 +0200

    Början av frågesport kåden

---
+++ Frågesport/Frågesport/Program.cs
@@ -0,0 +1,35 @@
+﻿int points = 0;
+Console.WriteLine("Vad heter du?");
+Console.WriteLine("Svar :");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Välkommen" + name + "till Vem vill bli en Miljonär");
+Console.WriteLine("Då börjar vi med första frågan.");
+Console.WriteLine("Hur länge har varit i production och televiserat.");
+Console.WriteLine("A = 1999 | B = 2000 | C = 2001");
+
+ = Console.ReadLine();
+
+if (val1.ToLower() == "a")
+{
+
+    Console.WriteLine("Tragiskt, Ses nästa gång!");
+
+}
+else if (val1.ToLower() == "b")
+{
+    Console.WriteLine("");
+
+}
+else (val1.ToLower() == c)
+{
+
+}
+
+
+
+
+
+
+Console.ReadLine();
